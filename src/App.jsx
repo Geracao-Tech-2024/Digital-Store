306110e7c5a2600a0425d05966eb5d4e22fb3e18
LUCAS MACIEL DOS SANTOS <LUCAS0721@DIGITALCOLLEGE.LOCAL>
2024-07-18 15:14:45 -0300

    adição de routas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import HomePage from './pages/HomePage/HomePage'
 import Layout from './pages/Layout/Layout'
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
 
   return (
-    <div> 
-        <Layout children={HomePage}/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route  path='/' element={<Layout children={HomePage} />} exact/>
+          <Route path='/sobre' element={<Layout children={HomePage} />}/>
+          {/* possivel melhoria PAGINA 404 */}
+          {/* <Route  path='/*' element={<Layout children={()=> (<><h1>404 NOT FOUND</h1></>)} />}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
