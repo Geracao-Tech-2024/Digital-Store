e22f9f9c64a35918a87708b0e2b547710cfe662e
WENDELL DE OLIVEIRA MONTEIRO <WENDELL0682@DIGITALCOLLEGE.LOCAL>
2024-07-18 15:00:12 -0300

    Adição componente CollectionIcons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import './App.css'
-import HomePage from './pages/HomePage/HomePage'
-import Layout from './pages/Layout/Layout'
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import Layout from "./pages/Layout/Layout";
 
 function App() {
-
-
   return (
-    <div> 
-        <Layout children={HomePage}/>
+    <div>
+      <Layout children={HomePage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
