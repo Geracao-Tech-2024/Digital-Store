0cda2a58523494578543fea7c865a373e93d4ec1
LUCAS MACIEL DOS SANTOS <LUCAS0721@DIGITALCOLLEGE.LOCAL>
2024-07-19 16:51:46 -0300

    gallery atualizado, componente para banner completo

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,20 +1,21 @@
 import './style.css'
+import styled from 'styled-components';
 import { useEffect, useState } from "react";
 
-export default function Gallery({ images }) {
-    const [index, setIndex] = useState(0); // Corrigido: índice inicial deve ser 0
-    const totalSlides = images.length; // Número total de slides
-    const slideWidth = 100; // Largura de cada slide em percentagem (ou em pixels, conforme necessário)
-
+export default function Gallery({ images, width, height, radius, showThumbs }) {
+    const [index, setIndex] = useState(0);
+    const totalSlides = images.length;
+    const slideWidth = 100;
     function nextSlide() {
-        setIndex((prevIndex) => (prevIndex + 1) % totalSlides); // Avançar para o próximo slide circularmente
-    }
-    function previewSlide() {
-        setIndex((prevIndex) => (prevIndex - 1) % totalSlides); // Voltar para o anterior slide circularmente
+        setIndex((prevIndex) => (prevIndex + 1) % totalSlides);
     }
 
-    function updateNavDots() {
-        return Array.from({ length: totalSlides }, (value , i) => (
+    function previewSlide() {
+        setIndex((prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides);
+    }
+
+    function viewNavDots() {
+        return Array.from({ length: totalSlides }, (value, i) => (
             <div
                 key={i}
                 className={`nav-dot ${index === i ? 'active' : ''}`}
@@ -26,39 +27,73 @@ export default function Gallery({ images }) {
     useEffect(() => {
         const slides = document.querySelector('.slides');
         slides.style.transform = `translateX(-${index * slideWidth}%)`;
-        updateNavDots();
     }, [index]);
 
+    const Banner = styled.div`
+        background-image: url(${props => props.src});
+        width: 100%; /* Ajuste conforme necessário */
+        background-size: cover; /* Ajuste conforme necessário */
+        background-position: center; /* Ajuste conforme necessário */
 
+        border-radius: ${radius};
+        width: ${width};
+        height: ${height};
+    `;
+
+    function showSlideBanner() {
+        return (
+            // <div className="slide" id="slide-0" key={0}>
+            //     <div className="item-slide">
+            //         <div className="container d-flex flex-row">
+            //             <div className="row">
+            //                 <div className="col-6 d-flex align-items-center">
+            //                     <div className="info">
+            //                         <p className="text-small-bold-warning">Melhores ofertas personalizadas</p>
+            //                         <h1>Queima de stoque Nike 🔥</h1>
+            //                         <p className="text-medium">Consequat culpa exercitation mollit nisi excepteur do do
+            //                             tempor laboris eiusmod
+            //                             irure consectetur.</p>
+            //                         <button className="btn-ofertas text-small-bold">Ver Ofertas</button>
+            //                     </div>
+            //                 </div>
+            //                 <div className="col-6 d-flex align-items-center justify-content-center">
+            //                     <img src={'sapato-banner.svg'} className="banner-img" alt="Nike Air" />
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+                <></>
+            // </div>
+        )
+    }
     return (
         <div className="slider">
             <div className="slides">
-                {images.map((image, index) => (
-                    <div className="slide" id="slide-1" key={index}>
-                        <div className="item-slide">
-                            <div className="container d-flex flex-row">
-                                <div className="row">
-                                    <div className="col-6 d-flex align-items-center">
-                                        <div className="info">
-                                            <p className="text-small-bold-warning">Melhores ofertas personalizadas</p>
-                                            <h1>Queima de stoque Nike 🔥</h1>
-                                            <p className="text-medium">Consequat culpa exercitation mollit nisi excepteur do do
-                                                tempor laboris eiusmod
-                                                irure consectetur.</p>
-                                            <button className="btn-ofertas text-small-bold">Ver Ofertas</button>
-                                        </div>
-                                    </div>
-                                    <div className="col-6 d-flex align-items-center justify-content-center">
-                                        <img src={image.src} className="banner-img" alt="Nike Air" />
+            {showThumbs != true? showSlideBanner() : console.log('Arquivos Carregados')}
+
+                {images.map((image, idx) => (
+                    <div className="slide" id={`slide-${idx + 1}`} key={idx + 1}>
+                        <Banner src={image.src}>
+                            <div className="item-slide">
+                                <div className="container d-flex flex-row">
+                                    <div className="d-flex align-items-center">
+                                        
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </Banner>
                     </div>
                 ))}
             </div>
             <div className="navigation">
-                {updateNavDots()}
+                {showThumbs != true? viewNavDots() : null}
+                <div className="previewSlides">
+                    <img src="produc-image-1.jpeg" className='item-produto active' alt="item produto" />
+                    <img src="produc-image-1.jpeg" className='item-produto' alt="item produto" />
+                    <img src="produc-image-1.jpeg" className='item-produto' alt="item produto" />
+                    <img src="produc-image-1.jpeg" className='item-produto' alt="item produto" />
+                    <img src="produc-image-1.jpeg" className='item-produto' alt="item produto" />
+                </div>
             </div>
         </div>
     );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,14 +4,23 @@ import Gallery from './../../components/Gallery/Gallery';
 // IMPORTANTE!: nome da classe em maiusculo
 function HomePage(){
   let imgs = [
-      {src: 'sapato-banner.svg'},
-      {src: 'sapato-banner.svg'},
-      {src: 'sapato-banner.svg'},
-      {src: 'sapato-banner.svg'}
+      // {src: 'home-slide-1.jpeg'},
+      // {src: 'home-slide-2.jpeg'},
+      // {src: 'home-slide-3.jpeg'},
+      // {src: 'home-slide-4.jpeg'},
+      // {src: 'home-slide-5.jpeg'},
+      // {src: 'home-slide-6.jpeg'},
+      // {src: 'home-slide-7.jpeg'},
+      // {src: 'home-slide-8.jpeg'}
+      {src: 'produc-image-1.jpeg'},
+      {src: 'produc-image-1.jpeg'},
+      {src: 'produc-image-1.jpeg'},
+      {src: 'produc-image-1.jpeg'},
+      {src: 'produc-image-1.jpeg'},
     ]
   return (
     <div>
-        <Gallery images={imgs}/>
+        <Gallery images={imgs} radius={'10px'} width={'50%'} height={'50%'} showThumbs />
     </div>
   );
 };
