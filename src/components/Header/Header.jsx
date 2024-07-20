9e0a92dee4f7e2118f353f097414db7162f3385c
LeviEstevao <estevao.01@icloud.com>
2024-07-20 00:21:41 -0300

    Process Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import "./HeaderStyle.css"
 
-// IMPORTANTE!: nome da classe em maiusculo
-function Header(){
+function Header() {
   return (
-    <div>
-        <h1>este é o header </h1>
-    </div>
+    <header className="boxheader">
+      <div className="container p-3">
+        <div className="row d-flex align-items-center">
+          <div className="col-10 d-flex justify-content-between">
+            <img src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="LogoDigital" className="logo" />
+            <input type="text" placeholder="Pesquise Aqui...." className="inputPesquisa" />
+            <a href="" className="text-dark d-flex align-items-center">Cadastra-se</a>
+          </div>
+
+          <div className="col">
+            <button className="estilobotton">Entrar</button>
+          </div>
+
+          <div className="col d-flex align-items-center px-2">
+            <img src="" alt="Carrinho Digital" className="iconCar"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col py-4">
+            <nav className="navbox">
+              <Link to="/Home">Home</Link>
+              <Link to="/Produtos">Produtos</Link>
+              <Link to="/Categorias"></Link>
+              <Link to="/Meus Produtos"></Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
