a5e804ed20b6270f3025dfa884d6e9cbc3fe7983
WENDELL DE OLIVEIRA MONTEIRO <WENDELL0682@DIGITALCOLLEGE.LOCAL>
2024-07-23 14:15:35 -0300

    adição do componente filterordenate

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-
-import './App.css'
-import HomePage from './pages/HomePage/HomePage'
-import Layout from './pages/Layout/Layout'
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import Layout from "./pages/Layout/Layout";
 import { Route, Routes } from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
-import PageProduct from './pages/PageProduct/PageProduct';
-
+import { BrowserRouter } from "react-router-dom";
+import PageProduct from "./pages/PageProduct/PageProduct";
 
 function App() {
   return (
     <main>
-
       <BrowserRouter>
         <Routes>
-          <Route  path='/' element={<Layout children={HomePage} />} exact/>
-          <Route  path='/produto' element={<Layout children={PageProduct} />} />
+          <Route path="/" element={<Layout children={HomePage} />} exact />
+          <Route path="/produto" element={<Layout children={PageProduct} />} />
         </Routes>
       </BrowserRouter>
-
     </main>
   );
 }
-
 
 export default App;
---
+++ src/components/FilterOrdenate/FilterOrdenate.jsx
@@ -0,0 +1,26 @@
+import "./FilterOrdenate.css";
+
+export default function FilterOrdenate() {
+  return (
+    <>
+      <div className="container">
+        <div className="resultado">
+          <h4>
+            Resultados para "Tênis" - <span>389 produtos</span>
+          </h4>
+        </div>
+
+        <div className="ordenacao">
+          <form action="#">
+            <label htmlFor="ordenate">Ordernar por:</label>
+            <select id="ordenate">
+              <option value="relevantes">mais relevantes</option>
+              <option value="MaiorPreco">maior preço</option>
+              <option value="MenorPreco">menor preço</option>
+            </select>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
