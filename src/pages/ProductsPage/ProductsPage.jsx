fbbe9c6b17ca295cf6801f25a44f62899ca27ffa
LUCAS MACIEL DOS SANTOS <LUCAS0721@DIGITALCOLLEGE.LOCAL>
2024-07-26 15:35:37 -0300

    Adicionando itens a lista do produtos

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -40,16 +40,39 @@ function ProductsPage() {
       price: 49.9,
     },
     {
-      name: "Nome do produto 2",
-
+      name: "Nome do produto 1",
+      image: "product-thumb-1.jpeg",
+      price: 200,
+      priceDiscount: 10,
+    },
+    {
+      name: "Nome do produto 1",
       image: "product-thumb-2.jpeg",
+      price: 200,
+      priceDiscount:30,
+    },
+    {
+      name: "Nome do produto 1",
+      image: "product-thumb-3.jpeg",
+      price: 200,
+      priceDiscount: 10,
+    },
+    {
+      name: "Nome do produto 1",
+      image: "product-thumb-4.jpeg",
+      price: 200,
+      priceDiscount: 20,
+    },
+    {
+      name: "Nome do produto 2",
+      image: "product-thumb-5.jpeg",
       price: 49.9,
     },
     {
       name: "Nome do produto 2",
-      image: "product-thumb-3.jpeg",
+      image: "product-thumb-1.jpeg",
       price: 49.9,
-    },
+    }
   ];
   const ProductsContainer = styled.div`
     background-color: var(--light-gray-3);
