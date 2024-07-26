a6e8dcbeaf700475732c7fdc74f108df0e916035
_w3ndell <academy.wendell@gmail.com>
2024-07-26 00:08:00 -0300

    criação do componente filtergroup

---
+++ src/components/FilterGroup/FilterGroup.jsx
@@ -0,0 +1,18 @@
+import "./FilterGroup.css";
+
+export default function FilterGroup({ tittle, options }) {
+  return (
+    <>
+      <div className="tittle-options">{tittle}</div>
+
+      <div className="filter-itens">
+        <input type="checkbox" name={options.text} id={options.text} />
+        <label htmlFor={options.text}>{options.text}</label>
+      </div>
+      <div className="filter-itens">
+        <input type="checkbox" name={options.text2} id={options.text2} />
+        <label htmlFor={options.text2}>{options.text2}</label>
+      </div>
+    </>
+  );
+}
